f98d6de766bee808ee66bfa625ba8e80f41ebce6
Saabitra Bera <63924604+Saabitra@users.noreply.github.com>
2023-04-14 22:09:34 +0530

    route.ts

---
+++ saabi-ai-img/app/api/suggestion/route.ts
@@ -0,0 +1,15 @@
+export async function GET(request: Request) {
+
+    // Connect to Microsoft Azure Function Endpoint
+    const response = await fetch('...', {
+        cache: 'no-store'
+    });
+
+    const textData = await response.text();
+
+
+    return new Response(JSON.stringify(textData.trim()), {
+        status: 200,
+    })
+  }
+  
